e9f61784ede4e14a71272ab9f3c73b0616d4b3dc
kaloykoy <kaloykoy@gmail.com>
2021-07-30 10:22:34 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+
+
+/*let year;
+console.log(typeof year);
+
+year = 1987;
+
+console.log(typeof year);
+
+const year = 2021;
+const ageZhoey = year - 2016;
+const ageHope = year - 2018;
+console.log(ageHope, ageZhoey);
+
+console.log(ageHope * 2, ageZhoey / 10, 2 ** 3);
+
+
+const firstName = 'carlo';
+const lastName = 'bernabe';
+console.log(firstName + ' ' + lastName);
+
+let xx = 10 + 4;
+xx += 10; //x = x +10
+xx *= 4; //x = x * 4
+xx++;
+alert(xx)
+
+console.log(ageZhoey < ageHope);
+console.log(ageHope >= 5);
+
+console.log(year - 2016 > year - 2018);
+
+let x, y;
+
+x = y = 25 - 10 - 5;
+console.log(x, y);
+
+const averageAge = (ageHope + ageZhoey) / 2;
+console.log(ageZhoey, ageHope, averageAge);
+
+
+
+
+
+const firstName = 'kaloy'
+const job = 'standby azzurro'
+const birthYear = 1987;
+const currentYr = 2021
+
+
+const kaloy = "i'm " + firstName + ', a ' + (currentYr - birthYear) + 'years old' + '!';
+
+console.log(kaloy);
+const newK = `I'm ${firstName}, a ${currentYr - birthYear} year old ${job}!`;
+console.log(newK);
+
+console.log('String with \n\ multiple \n\ lines');
+
+//USING \n\
+
+console.log(`String with
+multiple 
+lines`);
+
+
+
+
+const age = 15;
+
+if (age >= 18) {
+    console.log('cha can have sex with me!')
+}
+
+else {
+    const yearsLeft = 18 - age;
+    console.log(`she can just suck my cock. wait another ${yearsLeft} years :p`);
+}
+
+*/
+
+//BMI CALCULATOR BASIC
+const heightMarkA = 1.69;
+const heightJohnA = 1.95;
+const weightMarkA = 78;
+const weightJohnA = 92;
+
+const heightMarkB = 1.88;
+const heightJohnB = 1.76;
+const weightMarkB = 95;
+const weightJohnB = 85;
+
+
+
+
+bmiMA = weightMarkA / (heightMarkA) ** 2;
+bmiJA = weightJohnA / (heightJohnA) ** 2;
+console.log('mark BMI is ' + bmiMA);
+console.log('john BMI is ' + bmiJA);
+
+let markHigherBMIA = bmiMA > bmiJA;
+console.log("Is mark's BMI Higher tha john? " + markHigherBMIA);
+
+bmiMB = weightMarkB / (heightMarkB) ** 2;
+bmiJB = weightJohnB / (heightJohnB) ** 2;
+console.log('mark BMI is ' + bmiMB);
+console.log('john BMI is ' + bmiJB);
+
+let markHigherBMIB = bmiMB > bmiJB;
+console.log("Is mark's BMI Higher tha john? " + markHigherBMIB);
